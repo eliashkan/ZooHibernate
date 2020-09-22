201900e47743a4dc854ad74cd56b5d5810487db4
Elias Khan <khan.elias@gmail.com>
2020-09-22 16:20:14 +0200

    BaseEntity id best it be Integer

--- src/main/java/com/realdolmen/model/BaseEntity.java
+++ src/main/java/com/realdolmen/model/BaseEntity.java
@@ -5,17 +5,13 @@ import javax.persistence.*;
 @MappedSuperclass
 public class BaseEntity {
 
-    private int id;
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", updatable = false, nullable = false)
-    public int getId() {
+    private Integer id;
+
+    public Integer getId() {
         return id;
     }
 
-    public BaseEntity setId(int id) {
-        this.id = id;
-        return this;
-    }
 }
