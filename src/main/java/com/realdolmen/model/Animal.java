0eadf5c36a03dd4cf5335e586c024ecd658adb40
Elias Khan <khan.elias@gmail.com>
2020-09-22 16:19:43 +0200

    biDirectional OneToMany impl

--- src/main/java/com/realdolmen/model/Animal.java
+++ src/main/java/com/realdolmen/model/Animal.java
@@ -1,15 +1,17 @@
 package com.realdolmen.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Objects;
 
 @Entity(name = "Animal")
+@Table(name = "animal", schema = "zooHibernate")
 public class Animal extends BaseEntity {
 
     private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private Food food;
 
-    @Column
     public String getName() {
         return name;
     }
@@ -19,7 +21,6 @@ public class Animal extends BaseEntity {
         return this;
     }
 
-    @Column
     public Food getFood() {
         return food;
     }
@@ -27,5 +28,21 @@ public class Animal extends BaseEntity {
     public Animal setFood(Food food) {
         this.food = food;
         return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Animal)) return false;
+        Animal animal = (Animal) o;
+        return getId() != null &&
+                Objects.equals(getName(), animal.getName()) &&
+                Objects.equals(getFood(), animal.getFood()) &&
+                Objects.equals(getId(), animal.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getFood(), getId());
     }
 }
--- src/main/java/com/realdolmen/model/Food.java
+++ src/main/java/com/realdolmen/model/Food.java
@@ -1,13 +1,24 @@
 package com.realdolmen.model;
 
-import javax.persistence.Column;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
-@Entity
+@Entity(name = "Food")
+@Table(name = "food", schema = "zooHibernate")
 public class Food extends BaseEntity {
     private String foodName;
 
-    @Column
+    @OneToMany(
+            mappedBy = "food",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Animal> animals = new ArrayList<>();
+
     public String getFoodName() {
         return foodName;
     }
@@ -15,5 +26,20 @@ public class Food extends BaseEntity {
     public Food setFoodName(String foodName) {
         this.foodName = foodName;
         return this;
+    }
+
+     /*
+         add and remove methods used to synchronize both sides of the bidirectional association.
+         You should always provide these methods whenever you are working with a bidirectional association as,
+         otherwise, you risk very subtle state propagation issues.
+     */
+    public void addFood(Animal animal) {
+        animals.add(animal);
+        animal.setFood(this);
+    }
+
+    public void removeFood(Animal animal) {
+        animals.remove(animal);
+        animal.setFood(null);
     }
 }
